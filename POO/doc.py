9921b873d86c3a2775ee2294e79fde30b774ee83
Asmortiz12~ <asm.ortiz@yavirac.edu.ec>
2022-12-02 13:06:06 -0500

    l

--- POO/doc.py
+++ POO/doc.py
@@ -1,1 +1,19 @@
-print('hola mundo')
+class persona:
+    nombre = str
+    apellido = str
+    
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+        
+    def imprimir(self):
+        print(self.nombre, self.apellido)    
+        
+x = persona("Alexander", "Flores")        
+x.imprimir()
+
+class estudiante(persona):
+    pass
+
+y = estudiante("Jeremi", "Cañizares")
+y.imprimir()
